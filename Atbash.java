2b2c5520b77011704461d3acdacebec28ae9f8e1
AdvaitNagvekar <142820001+AdvaitNagvekar@users.noreply.github.com>
2023-08-21 22:19:27 +0530

    Update Atbash.java

--- Atbash.java
+++ Atbash.java
@@ -8,27 +8,16 @@ public class Atbash {
         String s1 = "";
         int i, k;
         char c;
+
         for (i = 0; i < s.length(); i++) {
             c = s.charAt(i);
             if (Character.isUpperCase(c))
                 c = (char) (90 + 65 - c);
-            else if (Character.isLowerCase(c))
+            if (Character.isLowerCase(c))
                 c = (char) (122 + 97 - c);
-            else
-                continue;
             s1 = s1 + c;
         }
-        /*
-         * for (i = 0; i < s.length(); i++) {
-         * if (Character.isUpperCase(s.charAt(i)))
-         * k = 90 + 65 - s.charAt(i);
-         * else if (Character.isLowerCase(s.charAt(i)))
-         * k = 122 + 97 - s.charAt(i);
-         * else
-         * k = s.charAt(i);
-         * s1 = s1 + s.substring(i, i + 1).replace(s.charAt(i), (char) k);
-         * }
-         */
         System.out.println("Atbash Ciper:" + s1);
     }
 }
+
